dbc9feb4ba29c5697e40c02bd16e72a1d25742a3
Satyajit Chaudhary <thisissatyajit05@gmail.com>
2024-07-10 17:56:14 +0530

    feat: release

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,184 +1,171 @@
-import SERVER_URL from "../../url.config"
-export async function saveUserDB(userInfo:any){      //saving user in db 
-      const { user } = userInfo;
-      const googleId = user.uid;
-      const name = user.displayName || '';
-      const email = user.email || '';
-      const emailVerified = user.emailVerified || false;
-      const picture = user.photoURL || '';
-      const providerId = user.providerId || '';
+import axios from 'axios';
+import SERVER_URL from '../../url.config';
 
-      // Construct request body with only necessary properties
-      const requestBody = {
-        googleId,
-        name,
-        email,
-        emailVerified,
-        picture,
-        providerId,
-      };
-      // Send API request to save user data
-      const response = await fetch(`http://${SERVER_URL}/api/v1/users/save`, {
-        method: 'POST',
+export async function saveUserDB(userInfo: any) {
+  const {user} = userInfo;
+  const googleId = user.uid;
+  const name = user.displayName || '';
+  const email = user.email || '';
+  const emailVerified = user.emailVerified || false;
+  const picture = user.photoURL || '';
+  const providerId = user.providerId || '';
+
+  const requestBody = {
+    googleId,
+    name,
+    email,
+    emailVerified,
+    picture,
+    providerId,
+  };
+
+  try {
+    const response = await axios.post(
+      `${SERVER_URL}/api/v1/users/save`,
+      requestBody,
+      {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(requestBody),
-      });
-
-      
-
-      const data = await response.json();
-      return data
-    }
-export async function fetchNews(categories: string[] = [], page: number = 1, limit: number = 3) {
-  try {
-    // Construct the base URL
-    let url = `http://${SERVER_URL}/api/v1/news?`;
-
-    // Add categories to the URL if provided
-    if (categories.length > 0) {
-      const categoriesParam = categories.map(cat => `categories=${encodeURIComponent(cat)}`).join('&');
-      url += `${categoriesParam}&`;
-    }
-
-    // Add page and limit parameters to the URL
-    url += `page=${page}&limit=${limit}`;
-    // Make the GET request
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
       },
-    });
+    );
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    return data;
+    return response.data;
   } catch (error) {
-    console.log('Error fetching news:', error);
+    console.error('Error saving user:', error);
     throw error;
   }
 }
-export async function saveStocks (googleId: string, stocks: any[]) {
+
+export async function fetchNews(
+  categories: string[] = [],
+  page: number = 1,
+  limit: number = 3,
+) {
   try {
-    const url = `http://${SERVER_URL}/api/v1/users/stocks`;
-    const response = await fetch(url, {
-      method: 'POST',
+    let url = `${SERVER_URL}/api/v1/news?`;
+
+    if (categories.length > 0) {
+      const categoriesParam = categories
+        .map(cat => `categories=${encodeURIComponent(cat)}`)
+        .join('&');
+      url += `${categoriesParam}&`;
+    }
+
+    url += `page=${page}&limit=${limit}`;
+
+    const response = await axios.get(url, {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({
-        googleId,
-        stocks,
-      }),
     });
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    return data
-    // Handle the response data as needed
+    return response.data;
   } catch (error) {
-    console.log('API call error:', error);
-    // Handle network errors or other exceptions
+    console.error('Error fetching news:', error);
+    throw error;
   }
-  
 }
-export async function searchNewsByTitle(queryText: string, page: number = 1, limit: number = 10): Promise<any[]> {
+
+export async function saveStocks(googleId: string, stocks: any[]) {
+  try {
+    const url = `${SERVER_URL}/api/v1/users/stocks`;
+
+    const response = await axios.post(
+      url,
+      {googleId, stocks},
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+
+    return response.data;
+  } catch (error) {
+    console.error('Error saving stocks:', error);
+    throw error;
+  }
+}
+
+export async function searchNewsByTitle(
+  queryText: string,
+  page: number = 1,
+  limit: number = 10,
+): Promise<any[]> {
   if (!queryText.trim()) {
     throw new Error('Search bar is empty');
   }
 
   const encodedQueryText = encodeURIComponent(queryText);
-  const url = `http://${SERVER_URL}/api/v1/news/search?searchText=${encodedQueryText}&page=${page}&limit=${limit}`;
+  const url = `${SERVER_URL}/api/v1/news/search?searchText=${encodedQueryText}&page=${page}&limit=${limit}`;
+
   try {
-    const response = await fetch(url, {
-      method: 'GET',
+    const response = await axios.get(url, {
       headers: {
         'Content-Type': 'application/json',
       },
     });
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    return data;
+    return response.data;
   } catch (error) {
-    console.log('Error searching news:', error);
+    console.error('Error searching news:', error);
     throw error;
   }
 }
+
 export async function fetchUserStocks(googleId: string): Promise<any> {
-  const url = `http://${SERVER_URL}/api/v1/users/userStocks/${googleId}`;
+  const url = `${SERVER_URL}/api/v1/users/userStocks/${googleId}`;
+
   try {
-    const response = await fetch(url, {
-      method: 'GET',
+    const response = await axios.get(url, {
       headers: {
         'Content-Type': 'application/json',
       },
     });
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    return data;
+    return response.data;
   } catch (error) {
-    console.log('Error fetching user stocks:', error);
+    console.error('Error fetching user stocks:', error);
     throw error;
   }
 }
-export async function fetchNewsByStocks(googleId: string, page: number = 1, limit: number = 3) {
+
+export async function fetchNewsByStocks(
+  googleId: string,
+  page: number = 1,
+  limit: number = 3,
+) {
   try {
-    // Construct the base URL
-    let url = `http://${SERVER_URL}/api/v1/news/stocks?userId=${googleId}&page=${page}&limit=${limit}`;
-    // Make the GET request
-    const response = await fetch(url, {
-      method: 'GET',
+    const url = `${SERVER_URL}/api/v1/news/stocks?userId=${googleId}&page=${page}&limit=${limit}`;
+
+    const response = await axios.get(url, {
       headers: {
         'Content-Type': 'application/json',
       },
     });
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    return data;
+    return response.data;
   } catch (error) {
-    console.log('Error fetching news:', error);
+    console.error('Error fetching news by stocks:', error);
     throw error;
   }
-  
 }
+
 export async function fetchStocklist() {
   try {
-    const response = await fetch(`http://${SERVER_URL}/api/v1/news/stocklist`,{
-      method:'GET',
-      headers:{
-        'Content-Type':'application/json'
-      }
-    });
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
+    const response = await axios.get(
+      `${SERVER_URL}/api/v1/news/stocklist`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    );
 
-    const data = await response.json();
-    return data;
-  }
-  catch(error){
-    console.log('Error fetching list:', error);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching stock list:', error);
     throw error;
   }
-  
 }
--- url.config.js
+++ url.config.js
@@ -1,3 +1,3 @@
 // config.dev.js
-const SERVER_URL = 'portfoliobrief-backend.onrender.com'; // Development server URL
+const SERVER_URL = 'https://portfoliobrief-backend.onrender.com'; // Development server URL
 export default SERVER_URL;
